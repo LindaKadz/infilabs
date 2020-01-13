913eec68bc6d0811444fad61e70c4a1d64d4e13c
Calvin Mokua <inficalvin@gmail.com>
2020-01-13 06:25:41 -0500

    Ci

--- config/test.exs
+++ config/test.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :infilabs, Infilabs.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "infilabs_test",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  database: System.get_env("POSTGRES_DB") ||  "infilabs_test",
+  hostname: hostname: if(System.get_env("CI"), do: "postgres", else: "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
